ed56f8e6784acd9f5423ed9bed0d0dacfd20371d
Joseph Clay <testingriven@gmail.com>
2015-05-12 11:13:49 -0500

    abstract out namespacing

---
+++ src/expiration.js
@@ -0,0 +1,40 @@
+var KEY = '__STORGE__';
+
+var attempt = function(fn) {
+    try {
+        return fn();
+    } catch(e) {}
+};
+
+var getExpirations = function(storage) {
+    var result = attempt(function() {
+        return storage.getItem(KEY);
+    });
+    return result === undefined || result === '' ? result : JSON.parse(result);
+};
+
+var setExpirations = function(storage, expirations) {
+    attempt(function() {
+        storage.setItem(KEY, JSON.stringify(expirations));
+    });
+};
+
+module.exports = function(storage) {
+    var expirations = getExpirations(storage) || {};
+
+    return {
+        expired: function(key) {
+            var hasExpired = key in expirations && expirations[key] >= Date.now();
+            if (!hasExpired) { return hasExpired; }
+
+            delete expirations[key];
+            setExpirations(storage, expirations);
+
+            return true;
+        },
+        set: function(key, duration) {
+            expirations[key] = Date.now() + (duration || 0);
+            setExpirations(storage, expirations);
+        }
+    };
+};
