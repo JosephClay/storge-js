dd7e8975498cbc8caab37d5fe0e92322b3f84aa9
Joseph Clay <testingriven@gmail.com>
2015-05-18 10:36:26 -0500

    build

--- storge.js
+++ storge.js
@@ -1,4 +1,4 @@
-/*! storge-js - v2.0.2 - 2015-05-18 %>
+/*! storge-js - v2.0.3 - 2015-05-18 %>
  * https://github.com/JosephClay/storge-js
  * Copyright (c) 2013-2015 ; License: MIT */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.storge = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -261,6 +261,10 @@ var keygen     = require(4);
 var tryItem    = require(7);
 var migrate    = require(5);
 
+var factory = function(storage, name, semver) {
+    return storgeRef(storage, name, semver);
+};
+
 /**
  * @param {Object} localStorage, sessionStorage
  * @param {String} namespace
@@ -272,7 +276,7 @@ storgeRef = module.exports = function storge(storage, namespace, semver) {
     var migration = migrate(storage, gen.space, gen.ver);
 
     var api = function(name, semver) {
-        return storgeRef(storage, name, semver);
+        return factory(storage, name, semver);
     };
 
     /**
